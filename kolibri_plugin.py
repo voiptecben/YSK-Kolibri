7b766a19383de9229be1c3fa043738270331e424
Ben Roberts <ben.roberts@liquidtelecom.com>
2021-12-19 17:58:48 +0000

    Update kolibri_plugin.py

--- kolibri_plugin.py
+++ kolibri_plugin.py
@@ -21,16 +21,16 @@ class DefaultThemeHook(theme_hook.ThemeHook):
             # specify primary and secondary brand colors
             theme_hook.BRAND_COLORS: {
                 theme_hook.PRIMARY: {
-                    theme_hook.COLOR_V50: "#f0e7ed",
-                    theme_hook.COLOR_V100: "#dbc3d4",
-                    theme_hook.COLOR_V200: "#c59db9",
-                    theme_hook.COLOR_V300: "#ac799d",
-                    theme_hook.COLOR_V400: "#996189",
-                    theme_hook.COLOR_V500: "#874e77",
-                    theme_hook.COLOR_V600: "#7c4870",
-                    theme_hook.COLOR_V700: "#6e4167",
-                    theme_hook.COLOR_V800: "#5f3b5c",
-                    theme_hook.COLOR_V900: "#4b2e4d",
+                    theme_hook.COLOR_V50: "#60bb46",
+                    theme_hook.COLOR_V100: "#60bb46",
+                    theme_hook.COLOR_V200: "#fdb813",
+                    theme_hook.COLOR_V300: "#fdb813",
+                    theme_hook.COLOR_V400: "#c9252b",
+                    theme_hook.COLOR_V500: "#f15922",
+                    theme_hook.COLOR_V600: "#7a411b",
+                    theme_hook.COLOR_V700: "#7a411b",
+                    theme_hook.COLOR_V800: "#6851a2",
+                    theme_hook.COLOR_V900: "#6851a2",
                 },
                 theme_hook.SECONDARY: {
                     theme_hook.COLOR_V50: "#e3f0ed",
@@ -50,7 +50,7 @@ class DefaultThemeHook(theme_hook.ThemeHook):
                 theme_hook.BACKGROUND: static("background.jpg"),
                 theme_hook.BACKGROUND_IMG_CREDIT: "Gerald Njoroge",
                 theme_hook.SCRIM_OPACITY: 0.7,
-                theme_hook.TITLE: "Young Scientists Kenya Kolibri",
+                theme_hook.TITLE: "Young Scientists Kenya",
                 theme_hook.TOP_LOGO: {
                     theme_hook.IMG_SRC: None,  # use default Kolibri bird
                     theme_hook.IMG_STYLE: "padding-left: 64px; padding-right: 64px; margin-bottom: 8px; margin-top: 8px",
@@ -58,11 +58,11 @@ class DefaultThemeHook(theme_hook.ThemeHook):
                 },
                 theme_hook.SHOW_POWERED_BY: True,
                 theme_hook.SHOW_TITLE: True,
-                theme_hook.SHOW_K_FOOTER_LOGO: False,
+                theme_hook.SHOW_K_FOOTER_LOGO: True,
             },
             # side-nav config
             theme_hook.SIDE_NAV: {
-                theme_hook.TITLE: "Young Scientists Kenya Kolibri",
+                theme_hook.TITLE: "YSK",
                 theme_hook.BRANDED_FOOTER: {},
                 theme_hook.SHOW_K_FOOTER_LOGO: True,
             },
