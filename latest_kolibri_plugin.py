392c93d9e4206b6420810dbe7651c9f9fc751553
Ben Roberts <ben.roberts@liquidtelecom.com>
2022-10-15 21:56:19 +0300

    Create latest_kolibri_plugin.py

---
+++ latest_kolibri_plugin.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.templatetags.static import static
+
+from kolibri.core import theme_hook
+from kolibri.plugins import KolibriPluginBase
+from kolibri.plugins.hooks import register_hook
+
+
+class DefaultThemePlugin(KolibriPluginBase):
+    pass
+
+
+@register_hook
+class DefaultThemeHook(theme_hook.ThemeHook):
+    @property
+    def theme(self):
+        return {
+            "signIn": {
+                "background": static("background.jpg"),
+                "backgroundImgCredit": "Gerald Njoroge",
+                "topLogo": {
+                    "style": "padding-left: 64px; padding-right: 64px; margin-bottom: 8px; margin-top: 8px",
+                },
+            },
+        }
